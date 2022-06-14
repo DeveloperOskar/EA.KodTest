da6244dfbccb09722b73aba9fed7abf3b291c803
Oskar <dev.oskar.eriksson@gmail.com>
2022-06-14 21:30:24 +0200

    Add project files.

---
+++ EA.KodTest/DataModels/PackageMeasures.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EA.KodTest.DataModels
+{
+    public class PackageMeasures
+    {
+        public decimal Weight { get; set; }
+        public decimal Length { get; set; }
+        public decimal Height { get; set; }
+        public decimal Width { get; set; }
+
+        public bool ValidatePackage() =>
+            Weight <= 20 && Length <=60 && Height <=60 && Width <=60;
+    }
+}
